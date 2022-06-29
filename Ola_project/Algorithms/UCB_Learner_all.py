05ec7f3ceac46cb5c46ac68c6341f86b76631c95
Christopher Volpi <100381052+FoxChris99@users.noreply.github.com>
2022-06-29 17:15:01 +0200

    Add files via upload

---
+++ Ola_project/Algorithms/UCB_Learner_all.py
@@ -0,0 +1,24 @@
+from Algorithms.Learner_Environment_all import *
+
+class UCB_all(Learner_all):
+    def __init__(self, n_arms):
+        super().__init__(n_arms)
+        self.expected_rewards = np.zeros([5,n_arms])
+        self.confidence = np.array(([np.inf] * n_arms,[np.inf] * n_arms,[np.inf] * n_arms,[np.inf] * n_arms,[np.inf] * n_arms))
+
+    def pull_arm(self):
+        if self.t < self.n_arms:
+            return np.array([self.t] * 5)
+        upper_conf = self.expected_rewards + self.confidence
+        idx = np.zeros(5)
+        for i in range(5):
+            idx[i] = np.random.choice(np.argwhere(upper_conf[i] == upper_conf[i].max()).reshape(-1))
+        return idx
+
+    def update(self, pulled_arm, reward):
+        self.t += 1
+        for i in range(5):
+            self.expected_rewards[i][pulled_arm[i]] = (self.expected_rewards[i][pulled_arm[i]]*(self.t-1)  + reward[i]) / self.t
+            n_samples = np.size(self.rewards_per_arm[i][pulled_arm[i]])
+            self.confidence[i][pulled_arm[i]] = (2 * np.log(self.t) / n_samples) ** 0.5 if n_samples > 0 else np.inf
+        self.update_observations(pulled_arm, reward)
