7edf383c2ca68d03f4bcbc77e8ff384d9f954487
lucamainini <57671317+lucamainini@users.noreply.github.com>
2022-08-16 11:30:55 +0200

    Add some comments

--- Ola_project/Algorithms/Learner_Environment.py
+++ Ola_project/Algorithms/Learner_Environment.py
@@ -1,38 +1,45 @@
 import numpy as np
 from Environment.E_commerce import *
 
+
 class Environment:
 
-    def __init__(self, n_arms, E_commerce, margins_matrix, num_users, binary_vector, fixed_alpha, fixed_weights, fixed_units):
+    def __init__(self, n_arms, E_commerce, margins_matrix, num_users, binary_vector, fixed_alpha, fixed_weights,
+                 fixed_units):
         self.n_arms = n_arms
         self.E = E_commerce
-        self.clicks_current_day = 0  #each day (round) they reset
-        self.purchases_current_day  = 0 #each day (round) they reset
+        self.clicks_current_day = 0  # each day (round) they reset
+        self.purchases_current_day = 0  # each day (round) they reset
         self.num_users = num_users
-        self.binary_vector = binary_vector #defines the class of users
-        self.fixed_alpha = fixed_alpha #flag for the alpha ratios uncertainity
+        self.binary_vector = binary_vector  # defines the class of users
+        self.fixed_alpha = fixed_alpha  # flag for the alpha ratios uncertainity
         self.margins_matrix = margins_matrix
         self.fixed_weights = fixed_weights
         self.daily_units = np.zeros(5)
         self.fixed_units = fixed_units
 
     def round(self, pulled_arm):
+        """For each product, it changes the price corresponding to the pulled arm
 
-        #for each product we change the price corresponding to the arm pulled
+        :param pulled_arm: vector with pulled prices
+        :type pulled_arm: ndarray
+        :return: reward
+        """
+
         for i in range(5):
             self.E.products[i].change_price(int(pulled_arm[i]))
 
-        #Reward is given thanks to the simulation of a day in the E-commerce website
+        # Reward is given thanks to the simulation of a day in the E-commerce website
         self.E.simulate_day(self.num_users, self.binary_vector, self.fixed_alpha, self.fixed_weights, self.fixed_units)
         self.clicks_current_day = self.E.daily_clicks
         self.purchases_current_day = self.E.daily_purchases
         self.daily_units = self.E.daily_purchased_units
-        
-        # reward for each product
+
+        # reward for each product is the sum of rewards per product / clicks on that product
         reward = np.zeros(5)
         for i in range(5):
-            if self.clicks_current_day[i]!=0:
-                reward[i] = self.E.daily_rewards_per_product[i]/self.clicks_current_day[i]
+            if self.clicks_current_day[i] != 0:
+                reward[i] = self.E.daily_rewards_per_product[i] / self.clicks_current_day[i]
         return reward
 
 
@@ -40,9 +47,9 @@ class Learner:
     def __init__(self, n_arms):
         self.n_arms = n_arms
         self.t = 0
-        self.rewards_per_arm = [[[0] for i in range(n_arms) ]for j in range(5)]
+        self.rewards_per_arm = [[[0] for i in range(n_arms)] for j in range(5)]
         self.collected_rewards = np.array([])
-        self.counter_per_arm = np.array([np.zeros(4)]*5)
+        self.counter_per_arm = np.array([np.zeros(4)] * 5)
 
     def update_observations(self, pulled_arm, reward):
         for i in range(5):
--- Ola_project/Algorithms/TS_Learner.py
+++ Ola_project/Algorithms/TS_Learner.py
@@ -1,20 +1,25 @@
 from Algorithms.Learner_Environment import *
 
+
 class TS(Learner):
     def __init__(self, n_arms):
         super().__init__(n_arms)
-        self.beta_parameters = np.array([np.ones((n_arms, 2))] * 5)       
-        self.expected_rewards = np.zeros([5,n_arms])
-        #self.mean_quantities = np.array([np.zeros(4)]*5) #mean quantities bought per product (in step 3 they are fixed)
+        self.beta_parameters = np.array([np.ones((n_arms, 2))] * 5)
+        self.expected_rewards = np.zeros([5, n_arms])
+        # self.mean_quantities = np.array([np.zeros(4)]*5)
+        # mean quantities bought per product (in step 3 they are fixed)
 
     def pull_arm(self, margins_matrix):
-        #First exploration of all arms (to get a first estimate of their conversion rate)
+        # First exploration of all arms (to get a first estimate of their conversion rate)
         if self.t < self.n_arms:
             return np.array([self.t] * 5)
         idx = np.zeros(5)
-        #We pull the arm with the best expected reward drawn from a beta with respect to the margins (lower conv rates have higer margins)
+        # We pull the arm with the best expected reward drawn from a beta with respect to the margins
+        # (lower conv rates have higher margins)
         for i in range(5):
-            idx[i] = np.argmax(np.random.beta(self.beta_parameters[i][:, 0], self.beta_parameters[i][:, 1]) * margins_matrix[i,:])#*self.mean_quantities[i]
+            idx[i] = np.argmax(
+                np.random.beta(self.beta_parameters[i][:, 0], self.beta_parameters[i][:, 1]) * margins_matrix[i, :])
+            #  *self.mean_quantities[i]
 
         return idx
 
@@ -22,11 +27,17 @@ class TS(Learner):
         self.t += 1
         self.update_observations(pulled_arm, reward)
         for i in range(5):
-            #we update the beta with the clicks and purchases of the day: estimation of the conversion rates
-            self.beta_parameters[i][int(pulled_arm[i]), 0] = self.beta_parameters[i][int(pulled_arm[i]), 0] + purchases[i]
-            self.beta_parameters[i][int(pulled_arm[i]), 1] = self.beta_parameters[i][int(pulled_arm[i]), 1] + (clicks[i] - purchases[i])
-            self.counter_per_arm[i][int(pulled_arm[i])] +=1
-            self.expected_rewards[i][int(pulled_arm[i])] = (self.expected_rewards[i][int(pulled_arm[i])] * (self.counter_per_arm[i][int(pulled_arm[i])]-1) + reward[i]) / self.counter_per_arm[i][int(pulled_arm[i])]
-            
-            #if purchases[i]!=0:
-                #self.mean_quantities[i][int(pulled_arm[i])] = (self.mean_quantities[i][int(pulled_arm[i])]* (self.counter_per_arm[i][int(pulled_arm[i])]-1) + daily_units[i]/purchases[i])/ self.counter_per_arm[i][int(pulled_arm[i])] if self.mean_quantities[i][int(pulled_arm[i])] > 0 else daily_units[i]/purchases[i]
+            # we update the beta with the clicks and purchases of the day: estimation of the conversion rates
+            self.beta_parameters[i][int(pulled_arm[i]), 0] = self.beta_parameters[i][int(pulled_arm[i]), 0] + purchases[
+                i]
+            self.beta_parameters[i][int(pulled_arm[i]), 1] = self.beta_parameters[i][int(pulled_arm[i]), 1] + (
+                    clicks[i] - purchases[i])
+            self.counter_per_arm[i][int(pulled_arm[i])] += 1
+            self.expected_rewards[i][int(pulled_arm[i])] = (self.expected_rewards[i][int(pulled_arm[i])] * (
+                    self.counter_per_arm[i][int(pulled_arm[i])] - 1) + reward[i]) / self.counter_per_arm[i][
+                                                               int(pulled_arm[i])]
+            # should be the same as = alpha/(alpha + beta)
+
+            # if purchases[i]!=0:
+            # self.mean_quantities[i][int(pulled_arm[i])] = (self.mean_quantities[i][int(pulled_arm[i])]*
+            # (self.counter_per_arm[i][int(pulled_arm[i])]-1) + daily_units[i]/purchases[i])/ self.counter_per_arm[i][int(pulled_arm[i])] if self.mean_quantities[i][int(pulled_arm[i])] > 0 else daily_units[i]/purchases[i]
