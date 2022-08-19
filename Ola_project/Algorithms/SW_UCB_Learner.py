7d42942c76f9f04f49470945d1a61bd58bca6e1c
lucamainini <57671317+lucamainini@users.noreply.github.com>
2022-08-19 12:41:53 +0200

    Add some correction

--- Ola_project/Algorithms/SW_UCB_Learner.py
+++ Ola_project/Algorithms/SW_UCB_Learner.py
@@ -4,8 +4,6 @@ from Algorithms.UCB_Learner import UCB
 import numpy as np
 
 
-
-
 class SW_UCB(UCB):
     def __init__(self, n_arms, window_size):
         """ Sliding-window UCB algorithm.
@@ -14,6 +12,7 @@ class SW_UCB(UCB):
         :param window_size: size of the window (constant)
         """
         super().__init__(n_arms)
+        self.rewards_per_arm = [[[] for i in range(n_arms)] for j in range(5)]
         '''
         self.expected_rewards = np.zeros([5, n_arms])
         self.confidence = np.array(([[np.inf] * n_arms] * 5))
@@ -26,19 +25,29 @@ class SW_UCB(UCB):
 
     def update(self, pulled_arm, reward):
         self.t += 1
+        self.update_observations(pulled_arm, reward)
+        print('-----------time ', self.t)
+        print('pulled ',pulled_arm)
         for i in range(5):
             # for each product i
             self.pulled_arms[i] = np.append(self.pulled_arms[i], pulled_arm[i])
             self.counter_per_arm[i][int(pulled_arm[i])] += 1
 
+            #print('time t:',self.t)
+            #print('expected rewards', self.expected_rewards)
+            #print('product ',i)
             for arm in range(self.n_arms):
-                # update arm
-                if arm == int(pulled_arm[i]):
-                    self.expected_rewards[i][arm] = np.mean(self.rewards_per_arm[i][arm])
+                n_samples = np.sum(self.pulled_arms[i][-self.window_size:-1] == arm)
+                #print('arm', arm, ', n', n_samples)
+                #print('rewards till now: ',self.rewards_per_arm[i][arm])
+                self.expected_rewards[i][arm] = np.mean(self.rewards_per_arm[i][arm][-n_samples:])
+                #print('expected rewards', self.expected_rewards)
 
-                n_samples = np.sum(self.pulled_arms[i][-self.window_size:] == arm)
-                self.confidence[i][arm] = (self.c * np.log(
+                if self.t < self.window_size:
+                    self.confidence[i][arm] = (self.c * np.log(
                         self.t) / n_samples) ** 0.5 if n_samples > 0 else np.inf
+                else:
+                    self.confidence[i][arm] = (self.c * np.log(
+                        self.window_size) / n_samples) ** 0.5 if n_samples > 0 else np.inf
 
 
-        self.update_observations(pulled_arm, reward)
